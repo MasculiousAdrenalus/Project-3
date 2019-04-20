6a344ee491fb5f42b45f958d2a0e4ca527ac5f5c
MasculiousAdrenalus <brycegossling@gmail.com>
2019-04-20 13:52:09 +1000

    no message

--- brycegossling_3C.m
+++ brycegossling_3C.m
@@ -21,7 +21,7 @@ accel_imu = IMU.DATAf(1:3,:)';
 omega_imu = IMU.DATAf(4:6,:)';
 omega_imu(:,3) = -omega_imu(:,3); %since we take yaw to be negative
 time_imu = double(IMU.times(:)-IMU.times(1))/10000;
-
+assignin('base','time_imu',time_imu);
 yaw_rate = -IMU.DATAf(6,:);% - mean( IMU.DATAf(6,1:5000))) );
 N_imu = length(time_imu);
 bias = -mean(IMU.DATAf(6,1:1000));
@@ -64,15 +64,15 @@ stdDevSpeed = 0.15;%0.4;%m/s
 std_rangeMeasurement = 0.16;%meters
 std_angleMeasurement = 0.5*pi/180;%radians
 
-P = zeros(3,3) ;
+P = zeros(4,4) ;
 
 Q = diag( [ (0.1)^2 ,(0.1)^2 , (2*pi/180)^2]) ;
-Q_u = diag([stdDevGyro stdDevSpeed]);
+P_u = diag([stdDevGyro stdDevSpeed]);
 
 R = diag([std_rangeMeasurement*std_rangeMeasurement*4,...
       std_angleMeasurement*std_angleMeasurement*4]);
-Xe = [ 0; 0; pi/2 ] ;  
-Xe_History = zeros(3,length(time_imu));
+Xe = [ 0; 0; pi/2;0 ] ;  
+Xe_History = zeros(4,length(time_imu));
   
 %%
 % --------------------------------------
@@ -204,11 +204,24 @@ for i = 2:N_imu-2             % in this example I skip some of the laser scans.
     end
     set(MyGUIHandles.plot2(1),'xdata',X(1:i),'ydata', Y(1:i));
     
-    J = [   [1,0,-dt*speed(i)*sin(Xe(3))];
-            [0,1,dt*speed(i)*cos(Xe(3))]; 
-            [ 0,0,1 ] ] ;
-    A = dt*[[speed(i)*cos(Xe(3)) 0]; [speed(i)*sin(Xe(3)) 0]; [0 1];];
-    P = J*P*J'+ (Q + A*Q_u*A');
+    J = [   [1,0,-dt*speed(i)*sin(Xe(3)),0];
+            [0,1,dt*speed(i)*cos(Xe(3)),0]; 
+            [ 0,0,1,0 ];
+            [ 0,0,0,1]] ;
+    A = dt*[[speed(i)*cos(Xe(3)) 0];
+            [speed(i)*sin(Xe(3)) 0];
+            [0 1];
+            [0 0];];
+        
+        Q_u = A*(P_u)*A';
+    % then I calculate the new coveraince, after the prediction P(K+1|K) = J*P(K|K)*J'+Q ;
+    Q1 = diag( [ (0.01)^2 ,...
+                        (0.01)^2 ,...
+                            (1*pi/180)^2,...
+                                0]) ;
+    Q = (Q1 + Q_u);
+    P = J*P*J'+ Q ;
+
     %Xe = [X(i); Y(i); yaw(i)];
     
 
@@ -242,16 +255,16 @@ for i = 2:N_imu-2             % in this example I skip some of the laser scans.
 	    eDD = sqrt( eDX*eDX + eDY*eDY ) ; 
 	
 	    % New 2D measurement matrix:
-	    H = [[  -eDX/eDD , -eDY/eDD , 0 ]; 
-		 [eDY/(eDX^2 + eDY^2), -eDX/(eDX^2 + eDY^2), -1]];
+	    H = [[  -eDX/eDD , -eDY/eDD , 0 ,0]; 
+		 [eDY/(eDX^2 + eDY^2), -eDX/(eDX^2 + eDY^2), -1,0]];
 
 	    ExpectedRange = eDD ;  
 	    ExpectedBearing = wrapToPi(atan2(eDY,eDX) - Xe(3) + pi/2)
         
         OOI.MeasuredRanges = [OOI.MeasuredRanges, sqrt( (OOI.global.y(k) - Xe(1))^2 + ...
                                 (OOI.global.y(k) - Xe(2))^2 ) ];
-        OOI.MeasuredBearings = [OOI.MeasuredBearings, atan2( (OOI.global.x(k) -Xe(1)),...
-                                (OOI.global.y(k) -Xe(2)))];
+        OOI.MeasuredBearings = [OOI.MeasuredBearings, wrapToPi(atan2( (OOI.global.y(k) -Xe(2) ),...
+                                                                       (OOI.global.x(k) -Xe(1) )))];
                             
 	    z = [OOI.MeasuredRanges(k) - ExpectedRange; OOI.MeasuredBearings(k) - ExpectedBearing]; 
 
@@ -262,27 +275,32 @@ for i = 2:N_imu-2             % in this example I skip some of the laser scans.
 % 	    R = diag([sdev_rangeMeasurement*sdev_rangeMeasurement*4,...
 % 		      sdev_angleMeasurement*sdev_angleMeasurement*4]);
 
-	    S = R + H*P*H' ;
-	    iS=inv(S);
-	    K = P*H'*iS ;           % Kalman gain
+        % Some intermediate steps for the EKF (as presented in the lecture notes)
+        S = R + H*P*H' ;
+        iS = inv(S);%iS=1/S;                 % iS = inv(S) ;   % in this case S is 1x1 so inv(S) is just 1/S
+        K = P*H'*iS ;           % Kalman gain
+        % ----- finally, we do it...We obtain  X(k+1|k+1) and P(k+1|k+1)
 
-	    % ----- finally, we do it...We obtain  X(k+1|k+1) and P(k+1|k+1)
-	    Xe = Xe+K*z            % update the  expected value
-	    P = P-P*H'*iS*H*P ;     % update the Covariance
+        Xe = Xe+K*z        % update the  expected value
+        %P = P-P*H'*iS*H*P;%P = P-K*H*P ;       % update the Covariance % i.e. "P = P-P*H'*iS*H*P"  )
+        P = P-K*H*P ;
         %set(MyGUIHandles.plot2(4),'xdata',Xe_History(1,:),'ydata', Xe_History(2,:));
+        %set(MyGUIHandles.plot3(2),'xdata',time_imu(1:i),'ydata', Xe_History(3,1:i));
+        %set(MyGUIHandles.plot2(4),'xdata',Xe_History(1,1:i),'ydata', Xe_History(2,1:i));
     end
     
     Xe_History(:,i) = Xe;
     assignin('base','Xe_History',Xe_History);
-    set(MyGUIHandles.plot3(2),'xdata',time_imu,'ydata', Xe_History(3,:));
-    set(MyGUIHandles.plot6,'xdata',Xe_History(1,:),'ydata', Xe_History(2,:));
+        %assignin('base', 'MyGUIHandles', MyGUIHandles);
+    
+    %set(MyGUIHandles.plot6,'xdata',Xe_History(1,:),'ydata', Xe_History(2,:));
     %set(handles.kalman_trace,'xdata',Xe_History(1,1:i),'ydata',Xe_History(2,1:i));
-    %set(MyGUIHandles.plot2(4),'xdata',Xe_History(1,:),'ydata', Xe_History(2,:));
     pause(0.0001);
     %i=i+skip;
 end
     set(MyGUIHandles.plot6,'xdata',Xe_History(1,:),'ydata', Xe_History(2,:));
     set(MyGUIHandles.plot6,'xdata',t,'ydata', Xe_History(3,:));
+    assignin('base', 'MyGUIHandles', MyGUIHandles);
     assignin('base', 'Xe_History', Xe_History);
     fprintf('\nDONE!\n');
 
@@ -290,10 +308,10 @@ return;
 end
 %%
 function Xnext=RunProcessModel(Xe,speed,GyroZ,dt) 
-    Xnext =     [ (dt*speed*cos(Xe(3)+Xe(1)));
-                   (dt*speed*sin(Xe(3)+Xe(2)));
-                   (GyroZ);
-                   ] ;
+    Xnext =  Xe  +[(dt*speed*cos(Xe(3)));
+                   (dt*speed*sin(Xe(3)));
+                   (GyroZ-Xe(4));
+                   0] ;
 return ;
 end
 %%
@@ -308,7 +326,7 @@ function OOI = AssociateIOO(OOI, H, t,q, Xe)
        dist_y = OOI.global.y - OOI.local.y(i);
        dist = sqrt(dist_x.^2 + dist_y.^2);
        [dist_min,id] = min(dist);
-       %OOI.local.id(i) = id;
+       OOI.local.id(i) = id;
         assignin('base','dist', dist);
         if dist_min < 0.4
             OOI.local.id(i) = id;
